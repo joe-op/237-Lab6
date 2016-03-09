186b19b896a2d5df036181c7d66c68b4f5b19b6b
Joe O. <joseph.opseth@my.uwrf.edu>
2016-03-08 18:53:34 -0600

    Finish tribonacci and main

--- 237-Lab6/cs237lab6.cpp
+++ 237-Lab6/cs237lab6.cpp
@@ -18,7 +18,7 @@ using namespace std;
 /*
 Reverses a string.
 Input: a const reference to a string
-Optional input: string, int
+Optional: string, int
 Output: a string
 */
 string reverse_rec(string const &original, string result = "", int n = 0);
@@ -30,7 +30,7 @@ Input: an integer
 Optional: 4 integers
 Output: An integer
 */
-int tribonacci(int n, int sum = 0, int n1 = 1, int n2 = 1, int n3 = 1);
+int tribonacci(int n, int x = 1, int n1 = 1, int n2 = 1, int n3 = 1);
 
 void pause_237(bool);
 
@@ -38,6 +38,7 @@ int Trib_Count;
 
 int main()
 {
+	Trib_Count = 0;
 	string input;
 	int n;
 
@@ -47,12 +48,24 @@ int main()
 	cout << "Reverse String: " << reverse_rec(input) << endl;
 
 	pause_237(true);
-
+	
 	cout << "Enter an integer n to find the nth Tribonacci number: ";
 	cin >> n;
 	cout << "Result: " << tribonacci(n) << endl;
-	cout << "Number of function calls: " << Trib_Count;
+	cout << "Number of function calls: " << Trib_Count << endl;
 
+	Trib_Count = 0;
+	cout << endl << "25th Tribonacci number: " << tribonacci(25) << endl;
+	cout << "Trib Count for this call only: " << Trib_Count << endl;
+	Trib_Count = 0;
+	cout << endl << "26th Tribonacci number: " << tribonacci(26) << endl;
+	cout << "Trib Count for this call only: " << Trib_Count << endl;
+	Trib_Count = 0;
+	cout << endl << "28th Tribonacci number: " << tribonacci(28) << endl;
+	cout << "Trib Count for this call only: " << Trib_Count << endl;
+	Trib_Count = 0;
+	cout << endl << "30th Tribonacci number: " << tribonacci(30) << endl;
+	cout << "Trib Count for this call only: " << Trib_Count << endl;
 
 	pause_237(true);
 
@@ -71,22 +84,32 @@ string reverse_rec(string const &original, string result, int n) {
 	}
 }
 
-int tribonacci(int n, int sum, int n1, int n2, int n3) {
+int tribonacci(int n, int x, int n1, int n2, int n3) {
 	Trib_Count++;
-	// Test for explicitly defined numbers
-	if (n <= 2) {
-		if (n == 2) {
-			return 1;
+	// Return when done (> to prevent overflow in case of bad call to function)
+	if (x >= n)
+		return n1 + n2 + n3;
+	else if (x <= 2) {
+		if (n >= 0) {
+			switch (n) {
+			case 0:
+				return 1;
+			case 1:
+				return 1;
+			case 2:
+				return 1;
+			default:
+				return tribonacci(n, 3, 1, 1, 1);
+			}
 		}
-		else if (n == 1) {
-			return 1;
-		}
-		else if (n == 0) {
-			return 1;
+		else {
+			return 0; // return 0 for bad n parameter
 		}
 	}
 	else {
-		return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+		// recursive step
+		// increment x and pass the next 3 tribonacci numbers
+		return tribonacci(n, x + 1, n2, n3, n1 + n2 + n3);
 	}
 }
 
